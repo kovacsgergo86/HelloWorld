9c3b1042a6e6d68ab43d65e409ba0a213e6abfe8
GERGO Kovacs <kovacs.gergo@icloud.com>
2019-09-07 12:01:07 +0200

    string argumentumok

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helló Vasvári");
+		//System.out.println("hossza: " + args.length);
+		
+		System.out.println("Vezetéknév: " + args[0]);
+		System.out.println("Keresztnév: " + args[1]);
+		System.out.println("Életkor: " + args[2]);
 	}
 
 }
